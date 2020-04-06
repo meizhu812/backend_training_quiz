c86cb7bf441a879140c2cec6d5b775ffb5fb07ef
Zhu Mei <meizhu812@outlook.com>
2020-04-06 17:52:18 +0800

    [MZ]-AC0-refactor-optimize console logic

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,10 @@
+import parking.InvalidInput;
+import parking.Regex;
+
 import java.util.Scanner;
 
 public class Application {
+    private static final Scanner SC = new Scanner(System.in);
 
     public static void main(String[] args) {
         operateParking();
@@ -20,22 +24,27 @@ public class Application {
     }
 
     private static void handle(String choice) {
-        Scanner scanner = new Scanner(System.in);
-        if (choice.equals("1")) {
-            System.out.println("请输入初始化数据\n格式为\"停车场编号1：车位数,停车场编号2：车位数\" 如 \"A:8,B:9\"：");
-            String initInfo = scanner.next();
-            init(initInfo);
-        } else if (choice.equals("2")) {
-            System.out.println("请输入车牌号\n格式为\"车牌号\" 如: \"A12098\"：");
-            String carInfo = scanner.next();
-            String ticket = park(carInfo);
-            String[] ticketDetails = ticket.split(",");
-            System.out.format("已将您的车牌号为%s的车辆停到%s停车场%s号车位，停车券为：%s，请您妥善保存。\n", ticketDetails[2], ticketDetails[0], ticketDetails[1], ticket);
-        } else if (choice.equals("3")) {
-            System.out.println("请输入停车券信息\n格式为\"停车场编号1,车位编号,车牌号\" 如 \"A,1,8\"：");
-            String ticket = scanner.next();
-            String car = fetch(ticket);
-            System.out.format("已为您取到车牌号为%s的车辆，很高兴为您服务，祝您生活愉快!\n", car);
+        switch (choice) {
+            case "1":
+                System.out.println("请输入初始化数据\n格式为\"停车场编号1：车位数,停车场编号2：车位数\" 如 \"A:8,B:9\"：");
+                String initInfo = SC.next();
+                init(initInfo);
+                break;
+            case "2": {
+                System.out.println("请输入车牌号\n格式为\"车牌号\" 如: \"A12098\"：");
+                String carInfo = SC.next();
+                String ticket = park(carInfo);
+                String[] ticketDetails = ticket.split(",");
+                System.out.format("已将您的车牌号为%s的车辆停到%s停车场%s号车位，停车券为：%s，请您妥善保存。\n", ticketDetails[2], ticketDetails[0], ticketDetails[1], ticket);
+                break;
+            }
+            case "3": {
+                System.out.println("请输入停车券信息\n格式为\"停车场编号1,车位编号,车牌号\" 如 \"A,1,8\"：");
+                String ticket = SC.next();
+                String car = fetch(ticket);
+                System.out.format("已为您取到车牌号为%s的车辆，很高兴为您服务，祝您生活愉快!\n", car);
+                break;
+            }
         }
     }
 
@@ -51,4 +60,10 @@ public class Application {
         return "";
     }
 
+    private static String getValidInput(String prompt, Regex regex) throws InvalidInput {
+        System.out.println(prompt);
+        String input = SC.next();
+        regex.validate(input);
+        return input;
+    }
 }
