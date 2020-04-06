5a91029452d72bc178d56e7bd7cda86c3bbd5274
Zhu Mei <meizhu812@outlook.com>
2020-04-06 17:41:36 +0800

    [MZ]-AC1-feat-created init call

--- src/main/java/parking/ParkingManager.java
+++ src/main/java/parking/ParkingManager.java
@@ -1,6 +1,8 @@
 package parking;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 public class ParkingManager implements AutoCloseable {
     private Connection connection;
@@ -14,8 +16,12 @@ public class ParkingManager implements AutoCloseable {
         statusRepo.setConnection(connection);
     }
 
+    public void initParkingPlaces(List<ParkingStatus> initPlaces) throws SQLException {
+        statusRepo.init(initPlaces);
+    }
+
     @Override
-    public void close(){
+    public void close() {
         connection = null;
         statusRepo.close();
     }
