21b38aa6de41cdeb02004faf486764a754e8fd5f
Zhu Mei <meizhu812@outlook.com>
2020-04-06 17:36:16 +0800

    [MZ]-AC1-feat-created ParkingStatus object

--- src/main/java/parking/ParkingStatus.java
+++ src/main/java/parking/ParkingStatus.java
@@ -1,4 +1,39 @@
 package parking;
 
+import repository.Key;
+
 public class ParkingStatus {
+    @Key
+    private String region;
+    @Key
+    private String serial;
+    @Key
+    private String plateNo;
+
+    public ParkingStatus() {
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public String getPlateNo() {
+        return plateNo;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public void setPlateNo(String plateNo) {
+        this.plateNo = plateNo;
+    }
 }
