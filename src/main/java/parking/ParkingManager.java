ba445c65e38969781c12c2927f005d026ffc64f5
Zhu Mei <meizhu812@outlook.com>
2020-04-06 17:39:15 +0800

    [MZ]-AC1-feat-created ParkingManager.java

--- src/main/java/parking/ParkingManager.java
+++ src/main/java/parking/ParkingManager.java
@@ -1,4 +1,22 @@
 package parking;
 
-public class ParkingManager {
+import java.sql.Connection;
+
+public class ParkingManager implements AutoCloseable {
+    private Connection connection;
+    private ParkingStatusRepo statusRepo = new ParkingStatusRepo();
+
+    public ParkingManager() {
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+        statusRepo.setConnection(connection);
+    }
+
+    @Override
+    public void close(){
+        connection = null;
+        statusRepo.close();
+    }
 }
