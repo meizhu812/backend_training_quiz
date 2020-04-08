5eab71466a8a5c1b9b1068cc3871804e82c23fc3
Zhu Mei <meizhu812@outlook.com>
2020-04-08 22:46:42 +0800

    [MZ]-AC0-feat-load connection params from file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,20 @@
 import parking.ParkingConsole;
 import parking.ScannerFilter;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class App {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, IOException {
+        Properties properties = new Properties();
+        properties.load(App.class.getResourceAsStream("jdbc.properties"));
+        System.out.println(properties);
         try (Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/parking_lot?serverTimezone=UTC",
-                "root", "root");
+                properties.getProperty("url"), properties.getProperty("user"), properties.getProperty("password"));
              ParkingConsole console = new ParkingConsole(new ScannerFilter())) {
             console.setConnection(connection);
             console.operateParking();
